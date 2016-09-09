a420f340d793e6926d112e09ee351ac836c02a9b
Larissa Gaulia <larissagaulia@gmail.com>
2016-09-08 21:37:40 -0300

    chore(setup): check hourly for spikes

--- test/spike-detector.js
+++ test/spike-detector.js
@@ -4,6 +4,7 @@
 var chai = require('chai');
 var moment = require('moment');
 var sinon = require('sinon');
+var _ = require('underscore')
 var spikeD;
 
 var assert = chai.assert;
@@ -48,12 +49,42 @@ describe('spike-detection', function () {
 
     assert.isUndefined(spikeD.getNormalTrafficDay(tomorrow));
     assert.equal(Object.keys(spikeD.getNormalTraffic()).length, 8);
-    spikeD.increment(moment().add(1, 'day'));
+    spikeD.increment(moment(tomorrow));
 
     assert.isTrue(spy.calledOnce);
     assert.isDefined(spikeD.getNormalTrafficDay(tomorrow));
     assert.equal(Object.keys(spikeD.getNormalTraffic()).length, 8, 'number of keys didn\'t change');
     assert.equal(spikeD.getNormalTrafficDay(tomorrow)[hour], 1);
+  });
+
+  it('set initial data', function () {
+    var numberOfDays = 7;
+    var normalTrafficTest = {};
+    requireSpikeDetection(numberOfDays); 
+
+    for(var day = 0; day <= numberOfDays; day++){
+      normalTrafficTest[moment().subtract(day, 'days').format('YYMMDD')]=
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
+    }
+
+    assert.equal(Object.keys(spikeD.getNormalTraffic()).length, 8);
+    spikeD.setNormalTraffic(normalTrafficTest);
+    assert.equal(Object.keys(spikeD.getNormalTraffic()).length, 8);
+    assert.equal(spikeD.getNormalTrafficDay(moment())[3], 3);
+  });
+
+  it('tests _incrementTo with new hour', function () {
+    var numberOfDays = 3;
+    requireSpikeDetection(numberOfDays);
+
+    var spy_log = sinon.spy(spikeD, 'logLastHourCount');
+    var spy_checkSpike = sinon.spy(spikeD, 'checkForSpike');
+
+    var oneHourFromNow = moment().add(1, 'hour');
+    spikeD._incrementTo(oneHourFromNow);
+    
+    assert.isTrue(spy_log.calledOnce);
+    assert.isTrue(spy_checkSpike.calledOnce);
   });
 
 });
